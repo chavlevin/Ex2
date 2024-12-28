c2ab943e951d6389aaed6cf35e4de3928d0e9d0d
chavlevin <chava.levin1@msmail.ariel.ac.il>
2024-12-28 23:05:59 +0200

    isform

--- src/Cell.java
+++ src/Cell.java
@@ -3,7 +3,7 @@ public class Cell {
 
     //This method checks if the String in cell is a number
 
-    boolean isNumber(String n) {
+    public static boolean isNumber(String n) {
         boolean ans = false;//Starting by assuming the string isn't a number
 
         boolean allDigits = true; //Checks if string n consists of only digits.
@@ -41,10 +41,82 @@ public class Cell {
     }
 
 
-
     //This method will check whether a string is text.
 
-    boolean isText (String t); {
-
+   public static boolean isText (String t) {
+       if (isNumber(t)) {
+           return false;//Return false if the string is a number because then it isn't a text
+       }
+       if (isForm(t)) {
+           return false;//Return false if the string is a valid formula because then it isn't a text
+       }
+       return true;//Otherwise, it is a text
     }
-}
+
+    //This method will check whether a string is a formula.
+
+    public static boolean isForm (String f) {
+        if (!f.startsWith("=")) {//A valid formula must start with an equals sign
+            return false;
+        }
+        f = f.substring(1); //To validate the rest of the string, we look at everything after the equals sign.
+
+        if (isNumber(f)) {
+            return true;//A standalone number is a valid formula
+        }
+
+        if (!balancedPar(f)) {//If there are parentheses in the formula, they must be balanced.
+            return false;
+        }
+
+        if (f.startsWith("(") && f.endsWith(")")) {
+            String content = f.substring(1, f.length() - 1); //If the entire formula string is inside parentheses, remove them to validate its contents.
+            return isForm("=" + content);
+        }
+
+        for (int i = 0; i < f.length(); i++) {
+            char o = f.charAt(i);
+
+            if (o == '+' || o == '-' || o == '*' || o == '/') { //Checking for valid operators in formula
+                String leftSide = f.substring(0, i);//Left side of the operator
+                String rightSide = f.substring(i, i + 1);//Right side of the operator
+
+                if (isForm("=" + leftSide) && isForm("=" + rightSide)) {
+                    return true;
+                }
+            }
+
+            if (!Character.isDigit(o) && o != '+' && o != '-' && o != '*' && o != '/' && o != '(' && o != ')') {
+                return false;//Any other character would be invalid
+            }
+        }
+        return false;
+    }
+
+    private static boolean balancedPar (String f) {//Checks if amount of parentheses are balanced
+        int balance = 0;
+
+        for(int i = 0; i<f.length();i++) {
+            char p = f.charAt(i);
+            if (p == '(') {
+                balance++;//Each time there is an open parentheses the balance goes up.
+            } else if (p == ')') {
+                balance--;//Each time there is a close parentheses the balance goes down.
+            }
+            if (balance < 0) {//If the balance is negative then the parentheses are not balanced.
+                return false;
+            }
+        }
+        return balance ==0;//Balance must be zero if the parentheses are balanced
+            }
+        }
+
+
+
+
+
+
+
+
+
+
